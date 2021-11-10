5a61034d5c3954b64c0184c3b32f93f282a63554
OvershotHavik <plavetzky@gmail.com>
2021-11-10 12:13:13 -0500

    Initial Commit

---
+++ Apple Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple Frameworks
+//
+//  Created by Steve Plavetzky on 11/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
